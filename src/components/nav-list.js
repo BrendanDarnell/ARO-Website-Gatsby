e2ac76ce76a02af8d4030609c4aba9999a2ae988
Brendan Darnell <brendan_darnell@hotmail.com>
2019-07-15 03:34:33 -0600

    Create toggle-menu component with react transition group

--- src/components/nav-list.js
+++ src/components/nav-list.js
@@ -1,21 +1,50 @@
 import React from 'react';
 // import { Link }	from 'gatsby';
-import AniLink from "gatsby-plugin-transition-link/AniLink"
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+
+import ToggleMenu from './toggle-menu.js';
 
 import styles from './nav-list.module.css';
 
 
 // <li className={styles.li}><AniLink to="/" cover direction="right" duration={2} bg="#5b58a5" className={styles.link}>Home</AniLink></li>
-export default function NavList(props) {
-	return (
-		<div className={styles.navListDiv}>
-			<p className={styles.navigationP}><i className="far fa-compass"></i> Navigation</p>
-			<ul className={styles.navList}>
-				<li className={styles.li}><AniLink to="/products" cover direction="right" duration={2} bg="#5b58a5" className={styles.link}>Products</AniLink></li>
-				<li className={styles.li}>Capabilities</li>
-				<li className={styles.li}>Resources</li>
-				<li className={styles.li}>Company</li>
-			</ul>
-		</div>
+export default class NavList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showContactMenu: false
+		}
+		this.toggleContactMenu = this.toggleContactMenu.bind(this);
+	}
+
+	toggleContactMenu() {
+		console.log('showContactMenu', this.state.showContactMenu);
+		this.setState(state => ({
+			showContactMenu: !state.showContactMenu
+		}));	
+	}
+	
+	render () {
+		return (
+			<div className={styles.navListDiv}>
+				<ul className={styles.navList}>
+					<li className={styles.mainLi}>
+						<ToggleMenu handleClick={this.toggleContactMenu} showMenu={this.state.showContactMenu} buttonName={'Products'} buttonClassNames={styles.navButton}/>
+					</li>
+					<li className={styles.mainLi}>
+						<button className={styles.navButton}>Markets</button>
+					</li>
+					<li className={styles.mainLi}>
+						<button className={styles.navButton}>Quality</button>
+					</li>
+					<li className={styles.mainLi}>
+						<button className={styles.navButton}>Company</button>
+					</li>
+					<li className={styles.mainLi}>
+						<button className={styles.navButton}>Get In Touch</button>
+					</li>
+				</ul>
+			</div>
 		);
+	}
 }
---
+++ src/components/toggle-menu.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import styles from './toggle-menu.module.css';
+
+export default function ToggleMenu(props) {
+	const menuAnimation = {
+		enter: styles.menuAnimationEnter,
+		enterActive: styles.menuAnimationEnterActive, 
+		exit: styles.menuAnimationExit,
+		exitActive: styles.menuAnimationExitActive
+	};
+
+	const items = ['mirrors', 'lenses'];
+
+	const menuItems = items.map((item, index) => {
+		return (
+			<li key={index}>
+				{item}
+			</li>
+		)
+	});
+
+	console.log('ToggleMenu', props.showMenu);
+	
+	return (
+		
+			<div className={styles.menuDiv}>
+				<button onClick={props.handleClick} className={props.buttonClassNames}>{props.buttonName}</button>
+				<CSSTransition in={props.showMenu} timeout={5000} classNames={menuAnimation} mountOnEnter={false} unmountOnExit={true}>
+					<ul className={`${styles.menu} ${styles.showMenu}`}>
+						{menuItems}
+					</ul>
+				</CSSTransition>
+			</div>
+	)
+}
